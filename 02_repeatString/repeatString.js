93703ebb8dd0516e67cedcec90e9a94fa3e94d8f
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-10 18:18:05 +0000

    Completes excercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, repeats) {
+  if (repeats < 0) {
+    return "ERROR";
+  }
 
+  let stringMultiplied = "";
+  for (let i = 0; i < repeats; i++) {
+    stringMultiplied += string;
+  }
+  return stringMultiplied;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
