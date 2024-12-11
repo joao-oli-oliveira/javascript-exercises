8d798946fbf506f3c53efc33375b6f9332a29dda
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-11 16:51:45 +0000

    Solves excercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  const chars = string.split("");
+  const revChars = chars.toReversed();
+  const revString = revChars.join("");
 
+  return revString;
+
+  /*
+chop string
+    length 
+
+    split()     !
+    substring() !!
+
+    .slice()
+reverse the order
+concatenate 
+    String() 
+
+*/
 };
-
+reverseString("123 456");
 // Do not edit below this line
 module.exports = reverseString;
