052a35c72b3c5b2f3f2d776491e29b491be55279
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-12 17:07:13 +0000

    The best possible solution for this

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...search) {
+  return array.filter((el) => !search.includes(el));
 };
+
+console.log(removeFromArray(["a", "ab", "c"], "a", "ab"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
