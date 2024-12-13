36dd2e48fc274429343283747f2ddb1f7e80d724
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-13 18:00:56 +0000

    Solves exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 && 0 === year % 100) {
+    return false;
+  }
+  if (0 === year % 400) {
+    return true;
+  }
+  if (0 === year % 4 && year < 100) {
+    return true;
+  }
+  if (0 === year % 4 && year % 100) {
+    return true;
+  }
+  return false;
 };
+
+/*
+
+    divisible by 400?
+    if yes, leap year
+
+    divisible by 4 & !100?
+    if yes, leap year
+    
+    divisible by 100 & !400 ?
+    if yes, not leap year
+
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
