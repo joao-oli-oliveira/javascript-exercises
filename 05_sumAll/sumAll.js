8339d4be46932d52136cddb86d69437e6c09591c
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-13 16:00:11 +0000

    Solves exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
+function sumAll(min, max) {
+  if (
+    min < 0 ||
+    max < 0 ||
+    isNaN(min) ||
+    isNaN(max) ||
+    !Number.isInteger(min) ||
+    !Number.isInteger(max)
+  ) {
+    return "ERROR";
+  }
+  if (max > min) {
+    return Array.from(
+      { length: Math.ceil(max - min + 1) },
+      (_, i) => min + i
+    ).reduce((acc, currVal) => acc + currVal);
+  }
+  if (max < min) {
+    return Array.from(
+      { length: Math.ceil(min - max + 1) },
+      (_, i) => max + i
+    ).reduce((acc, currVal) => acc + currVal);
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
