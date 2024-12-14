d69dd24c487960e539b7cbc6ad2ef5c28ecae5ef
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-14 16:09:31 +0000

    Solves exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  const rawTempC = ((tempF - 32) * 5) / 9;
+  return Math.round(rawTempC * 10) / 10;
 };
+console.log(convertToCelsius(69));
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  const rawTempF = tempC * (9 / 5) + 32;
+  return Math.round(rawTempF * 10) / 10;
 };
+console.log(convertToFahrenheit(51));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
