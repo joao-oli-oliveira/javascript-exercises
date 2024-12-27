7998f106acbe4d117a922abdac32585b9030119a
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-27 18:21:40 +0000

    Solves exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  return (
+    string
+      .toLowerCase()
+      .split("")
+      .filter((char) => /[a-zA-Z0-9]/.test(char))
+      .reverse()
+      .toString() ===
+    string
+      .toLowerCase()
+      .split("")
+      .filter((char) => /[a-zA-Z0-9]/.test(char))
+      .toString()
+  );
 };
 
 // Do not edit below this line
