7f3e73adebad98f310380d6380c6642ac4951503
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-27 17:45:30 +0000

    Solves excercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const sumOfAllNums = arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return sumOfAllNums;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n > 0) {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
